82be1968e4e3f26201fde5aaef884793f95e5a33
BattyBoyBlue <byte2121@proton.mr>
2023-07-16 20:22:03 -0400

    First Commit

---
+++ src/main/java/net/batty/battystuf/BattyStuffClient.java
@@ -0,0 +1,10 @@
+package net.batty.battystuf;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BattyStuffClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
